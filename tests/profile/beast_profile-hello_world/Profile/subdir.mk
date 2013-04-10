bad7e1b43285af5b4fa4a7c00095d0b622c963b2
Miguel Palhas <mpalhas@gmail.com>
2013-04-10 17:42:19 +0100

    updated cprojects

--- tests/profile/beast_profile-hello_world/Profile/subdir.mk
+++ tests/profile/beast_profile-hello_world/Profile/subdir.mk
@@ -17,7 +17,7 @@ CPP_DEPS += \
 %.o: ../%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -D_Profile -I"/home/naps62/projects/beast" -I"/home/naps62/projects/beast/include" -O0 -g3 -Wall -c -fmessage-length=0 -std=c++0x -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -D_Profile -I"/home/naps62/projects/msc-thesis/src/beast" -I"/home/naps62/projects/msc-thesis/src/beast/include" -O0 -g3 -Wall -c -fmessage-length=0 -std=c++0x -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
